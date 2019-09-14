7ae58988371219b68054ba2d44952b73bb3fa151
Osasumwen Usen <osas.usen@gmail.com>
2019-09-14 03:29:04 +0100

    added more flesh to the project

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref, sessionmaker, joinedload
+from datetime import date
+
+from models.users import Person
+from models.base import session_factory
+
+from .scraper import ScrapeBot
 
 
-def initialize_db():
-    engine = create_engine('sqlite:///:memory:', echo=True)
+class ScrapeUsers:
+
+    def __init__(self):
+        self.session = session_factory()
+
+    def __get_users(self):
+        users = self.session.query(Person).filter_by(is_scraped=0)
+        self.session.close()
+        return users.all()
+
+    def run(self):
+        users = self.__get_users()
+        for user in users:
+            ScrapeBot(url=user.twitter_page_url).run()
