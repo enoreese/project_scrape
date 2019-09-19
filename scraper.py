1d517bd13bf305ec8d22f8091620f6bc14b6ce79
Osasumwen Usen <osas.usen@gmail.com>
2019-09-19 21:31:36 +0100

    headless

--- scraper.py
+++ scraper.py
@@ -52,10 +52,10 @@ class UpdateBot(object):
 
     def __init__(self, handle, headless=True):
         self.locator_dictionary = TwitterLocator.__dict__
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors"])
-        options.add_argument('--disable-gpu')
+        options = Options()
         options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
         self.browser = webdriver.Chrome(chrome_options=options, executable_path='driver/chromedriver')  # export PATH=$PATH:/path/to/chromedriver/folder
         logger.info("Updating DB for handle: {}".format(handle))
         url = "https://twitter.com/{}".format(str(handle))
--- scrapeusers.py
+++ scrapeusers.py
@@ -49,10 +49,10 @@ class ScrapeBot(object):
 
     def __init__(self, hashtag, headless=True):
         self.locator_dictionary = TwitterLocator.__dict__
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option("excludeSwitches", ["ignore-certificate-errors"])
-        options.add_argument('--disable-gpu')
+        options = Options()
         options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
         self.browser = webdriver.Chrome(chrome_options=options, executable_path='driver/chromedriver')  # export PATH=$PATH:/path/to/chromedriver/folder
         url = "https://twitter.com/search?q=%23{}&src=tyah".format(hashtag)
         logger.info("Scraping hashtag: {}".format(hashtag))
