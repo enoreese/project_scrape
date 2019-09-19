c0030b8a9fc6f8d73213984bace8512a76fc920f
Osasumwen Usen <osas.usen@gmail.com>
2019-09-19 04:35:32 +0100

    no sandbox option for selenium

--- scraper.py
+++ scraper.py
@@ -51,10 +51,10 @@ class UpdateBot(object):
 
     def __init__(self, handle, headless=True):
         self.locator_dictionary = TwitterLocator.__dict__
-        options = webdriver.ChromeOptions()
-        options.addArguments("--headless")
-        options.addArguments("--no-sandbox")
-        options.addArguments("--disable-dev-shm-usage")
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
         self.browser = webdriver.Chrome(options=options, executable_path='driver/chromedriver')  # export PATH=$PATH:/path/to/chromedriver/folder
         logger.info("Updating DB for handle: {}".format(handle))
         url = "https://twitter.com/{}".format(str(handle))
--- scrapeusers.py
+++ scrapeusers.py
@@ -51,10 +51,10 @@ class ScrapeBot(object):
 
     def __init__(self, hashtag, headless=True):
         self.locator_dictionary = TwitterLocator.__dict__
-        options = webdriver.ChromeOptions()
-        options.addArguments("--headless")
-        options.addArguments("--no-sandbox")
-        options.addArguments("--disable-dev-shm-usage")
+        options = Options()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
         self.browser = webdriver.Chrome(options=options, executable_path='driver/chromedriver')  # export PATH=$PATH:/path/to/chromedriver/folder
         url = "https://twitter.com/search?q=%23{}&src=tyah".format(hashtag)
         logger.info("Scraping hashtag: {}".format(hashtag))
