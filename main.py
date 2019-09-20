68f77c2b6aa8511663aa593031937403a7fa636e
Osasumwen Usen <osas.usen@gmail.com>
2019-09-20 01:16:41 +0100

    fix scrolling

--- main.py
+++ main.py
@@ -43,16 +43,16 @@ class ScrapeUsers:
 
     def run(self):
         logger.info("Starting Handles Scraper in Parallel")
-        self.scrape()
+        # self.scrape()
         # th.Thread(target=self.scrape()).start()
-        # scrape_handles = mp.Process(target=self.scrape)
-        # scrape_handles.start()
+        scrape_handles = mp.Process(target=self.scrape)
+        scrape_handles.start()
 
         logger.info("Starting Update Scraper in Parallel")
-        self.update()
+        # self.update()
         # th.Thread(target=self.update()).start()
-        # update_users = mp.Process(target=self.update)
-        # update_users.start()
+        update_users = mp.Process(target=self.update)
+        update_users.start()
         #
         # scrape_handles.join()
         #
